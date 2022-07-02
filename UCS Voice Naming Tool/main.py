52634951fa5f90e345371ce18e677bc419cfe1f6
Jon Evans <jonnythejet@gmail.com>
2022-07-02 00:54:01 -0700

    - Updated MaindWindow UI - Restructed files

--- UCS Voice Naming Tool/main.py
+++ UCS Voice Naming Tool/main.py
@@ -1,4 +1,14 @@
 
 #
+import sys
 
+from src.ui import UIMainWindow
+from PyQt5.QtWidgets import *
+
+obj = UIMainWindow.MainWindow()
+obj.show()
 print('Hello World!')
+
+
+app = QApplication([])
+sys.exit(app.exec_())
--- UCS Voice Naming Tool/src/ui/UIMainWindow.py
+++ UCS Voice Naming Tool/src/ui/UIMainWindow.py
@@ -8,8 +8,17 @@
 #
 #
 
-import PyQt5
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+from os import path
 
 
-class MainWindow:
-    pass
+class MainWindow(QMainWindow):
+    def __int__(self, **kwargs):
+
+        super(MainWindow, self).__init__()
+        uic.loadUi('/gui/MainWindow.ui')
+        self.show()
+
